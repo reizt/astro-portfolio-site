288fdb0f82b85b2fa18d3042d0c1b1ff20a151f9
reizt <reizt.dev@gmail.com>
2023-03-22 21:46:37 +0900

    Add page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,14 @@ import { Header } from '#/components/Header';
 import { SkillsSection } from '#/components/SkillsSection';
 import { TopSection } from '#/components/TopSection';
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
     <div className="px-16 xl:px-60 2xl:px-60">
+      <Head>
+        <title>Reiju Takahashi</title>
+      </Head>
       <Header />
       <TopSection />
       <CareerSection />
