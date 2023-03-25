e112f62250997768a01c133684f8b4e9876698c9
reizt <reizt.dev@gmail.com>
2023-03-25 16:02:20 +0900

    update: Update career

--- src/components/CareerSection.tsx
+++ src/components/CareerSection.tsx
@@ -9,6 +9,11 @@ export const CareerSection: FC<Props> = () => {
       <h2 className="mb-20 text-32 font-bold leading-tight tracking-widest xl:text-48">{'Career'}</h2>
       <div className="relative grow pt-48">
         <div className="absolute inset-y-0 left-0 h-full w-4 rounded-2 bg-black-700"></div>
+        <div style={{ minHeight: '50px' }}>
+          <HistoryItem month={new Date(2021, 3)}>
+            Entered <span className="text-blue">Tokyo University of Science</span>, Department of Information Engineering
+          </HistoryItem>
+        </div>
         <div style={{ minHeight: '300px' }}>
           <HistoryItem month={new Date(2021, 5)}>
             Joined <span className="text-blue">Creators Team</span> as an outsourced member
@@ -16,16 +21,24 @@ export const CareerSection: FC<Props> = () => {
         </div>
         <div style={{ minHeight: '75px' }}>
           <HistoryItem month={new Date(2022, 5)}>
-            Stopped to use Ruby on Rails and started to learn <span className="text-blue">TypeScript</span> and React
+            Started to learn <span className="text-blue">TypeScript</span> and React
           </HistoryItem>
         </div>
-        <div style={{ minHeight: '150px' }}>
+        <div style={{ minHeight: '100px' }}>
           <HistoryItem month={new Date(2022, 8)}>
             Appointed as <span className="text-blue">CTO</span> in Creators Team
           </HistoryItem>
         </div>
+        <div style={{ minHeight: '50px' }}>
+          <HistoryItem month={new Date(2023, 0)}>
+            Joined a internship program <span className="text-blue">Tech Accel</span> of <span className="text-blue">Cyber Agent</span>
+          </HistoryItem>
+        </div>
+        <div style={{ minHeight: '25px' }}>
+          <HistoryItem month={new Date(2023, 2)}>End the internship</HistoryItem>
+        </div>
         <div style={{ minHeight: '150px' }}>
-          <HistoryItem month={new Date(2023, 2)}>
+          <HistoryItem month={new Date(2023, 3)}>
             Developing <span className="text-blue">Werp</span>...
           </HistoryItem>
         </div>
