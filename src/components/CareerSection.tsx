123080d2aac55ed25bf96fba4bde82c1f483b458
reizt <reizt.dev@gmail.com>
2023-03-27 04:35:10 +0900

    update: Modify position of HistoryItem

--- src/components/CareerSection.tsx
+++ src/components/CareerSection.tsx
@@ -9,35 +9,35 @@ export const CareerSection: FC<Props> = () => {
       <h2 className="mb-20 text-32 font-bold leading-tight tracking-widest xl:text-48">{'Career'}</h2>
       <div className="relative grow pt-48">
         <div className="absolute inset-y-0 left-0 h-full w-4 rounded-2 bg-black-700"></div>
-        <div style={{ minHeight: '50px' }}>
+        <div className="mb-20" style={{ minHeight: '4rem' }}>
           <HistoryItem month={new Date(2021, 3)}>
             Entered <span className="text-blue">Tokyo University of Science</span>, Department of Information Engineering
           </HistoryItem>
         </div>
-        <div style={{ minHeight: '300px' }}>
+        <div className="mb-20" style={{ minHeight: '29rem' }}>
           <HistoryItem month={new Date(2021, 5)}>
             Joined <span className="text-blue">Creators Team</span> as an outsourced member
           </HistoryItem>
         </div>
-        <div style={{ minHeight: '75px' }}>
+        <div className="mb-20" style={{ minHeight: '6.5rem' }}>
           <HistoryItem month={new Date(2022, 5)}>
             Started to learn <span className="text-blue">TypeScript</span> and React
           </HistoryItem>
         </div>
-        <div style={{ minHeight: '100px' }}>
+        <div className="mb-20" style={{ minHeight: '9rem' }}>
           <HistoryItem month={new Date(2022, 8)}>
             Appointed as <span className="text-blue">CTO</span> in Creators Team
           </HistoryItem>
         </div>
-        <div style={{ minHeight: '50px' }}>
+        <div className="mb-20" style={{ minHeight: '4rem' }}>
           <HistoryItem month={new Date(2023, 0)}>
             Joined a internship program <span className="text-blue">Tech Accel</span> of <span className="text-blue">Cyber Agent</span>
           </HistoryItem>
         </div>
-        <div style={{ minHeight: '25px' }}>
+        <div className="mb-20" style={{ minHeight: '1.5rem' }}>
           <HistoryItem month={new Date(2023, 2)}>End the internship</HistoryItem>
         </div>
-        <div style={{ minHeight: '150px' }}>
+        <div className="mb-20" style={{ minHeight: '14rem' }}>
           <HistoryItem month={new Date(2023, 3)}>
             Developing <span className="text-blue">Werp</span>...
           </HistoryItem>
--- src/components/HistoryItem.tsx
+++ src/components/HistoryItem.tsx
@@ -12,7 +12,7 @@ export const HistoryItem: FC<Props> = ({ month, children, className = '', ...res
   return (
     <div className={`group relative flex flex-col pl-32 xl:flex-row xl:items-center ${className}`} {...restProps}>
       <i
-        className={`absolute top-1/2 -left-6 flex h-16 w-16 -translate-y-1/2 items-center justify-center rounded-full bg-blue shadow-dot shadow-blue duration-150 group-hover:-left-10 group-hover:h-24 group-hover:w-24`}
+        className={`absolute top-0 -left-6 flex h-16 w-16 items-center justify-center rounded-full bg-blue shadow-dot shadow-blue duration-150 group-hover:-left-10 group-hover:h-24 group-hover:w-24 sm:top-1/2 sm:-translate-y-1/2`}
       >
         <i className={`h-12 w-12 rounded-full border-2 border-black bg-blue duration-150 group-hover:h-20 group-hover:w-20`}></i>
       </i>
